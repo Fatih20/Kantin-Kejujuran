12dc9e1f46ca05ca012befd7837841b689e2cfb1
Fatih20 <fnri39@protonmail.com>
2022-07-02 16:09:22 +0700

    Correct typo in backend address.

--- src/config.ts
+++ src/config.ts
@@ -3,4 +3,4 @@ export const maxNameLength = 40;
 export const maxDescriptionLength = 100;
 export const initialSortCondition = [true, true];
 export const validImageExtensionList = ["png", "jpg", "gif", "jpeg"] as const;
-export const backendAddress = "https://honest-canteen-backend.herokuapp.com/";
+export const backendAddress = "https://honest-canteen-backend.herokuapp.com";
