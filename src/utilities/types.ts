7e5659c754ad1fa40e064c29b6fdc110c67b147f
Fatih20 <fnri39@protonmail.com>
2022-07-03 01:16:30 +0700

    Include ID from database to the SoldItem interface.

--- src/utilities/types.ts
+++ src/utilities/types.ts
@@ -13,7 +13,7 @@ export type PossibleInputProblem = typeof possibleInputProblemList[number];
 
 export interface ISoldItemLite {
     name : string,
-    image : File,
+    imageLink : File,
     description : string,
     price : number,
 
@@ -22,6 +22,10 @@ export interface ISoldItemLite {
 export interface ISoldItem extends ISoldItemLite {
     milisecondCreated : number,
     dateCreated : string,
+}
+
+export interface ISoldItemRaw extends ISoldItem {
+    id : number
 }
 
 const possibleNameProblemList = ["none", "long", "empty"] as const;
